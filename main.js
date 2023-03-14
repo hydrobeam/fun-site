31c60f635d694be9aa08f9c579230fd7ec65df74
Laith Bahodi <laithbahodi@gmail.com>
2023-03-13 20:46:05 -0400

    feat!: abstract tooltip system
    - there's a queryWikiMap class which points to classes that extend the
    BaseWikiAPICall class.
    - to extend the class and create a functioning source, some methods need
    to be overwritten (these are documented)
    - rewrote the rest of the listener stuff to accomodate this change
    - website now shows two query sources, for wikipedia and for runescape
    - wiki-links are now not selectable, for mobile users. also made them <p>'s

--- main.js
+++ main.js
@@ -1,3 +1,145 @@
+// To implement this base class, you need to override four methods:
+// 1. generateQueryUrk
+// - something you can simply fetch and get a response from
+// 2. extractSpecificObject
+// - when getting the json object from your call, return the data for the query.
+// 3. extractExtract
+// - get the summary information from your API call
+// 4. extractThumbnail
+// - get the thumbnail src from your API call
+// 5. extractQueryUrl
+// - get the url of the thing you're pointing to
+class BaseWikiAPICall {
+  constructor(url) {
+    this.url = url;
+    this.summaryHTML;
+    this.thumbnailSrc;
+    this.queryUrl;
+  }
+
+  // returns a URLSearchParams
+  generateQueryUrl(query){};
+
+  // returns an object
+  extractSpecificObject (responseJson){};
+
+  // returns a string
+  extractExtract(responseObject){};
+
+  // returns a string
+  extractThumbnail(responseObject){};
+
+  extractQueryUrl(responseObject){};
+
+  // returns URLSearchParams object
+  // takes  string
+  /* accepts string */
+  async generateJsonResponse(query) {
+    let response = await fetch(this.generateQueryUrl(query));
+    const responseJson = await response.json();
+    const responseObject = this.extractSpecificObject(responseJson);
+
+    const extract = this.extractExtract(responseObject);
+    if (extract) {
+      // FIXME: find better solution???
+      // replace breaking newline, just one should be enough... i guess
+      // do this because newlines aren't consistent with the rest of the line heights and introduce a weird
+      // clipping effect. this allows to have (hopefully) all extracts end at 9 lines
+      // JANK
+      // idk how the rswiki does it, they get plaintext but their thing has formatting... probably some preprocessing..
+      // in plaintext this isn't a problem since it ignores all newlines
+      this.summaryHTML = extract.replace('\n</p><p>', ' ');
+      this.thumbnailSrc = this.extractThumbnail(responseObject) ?? '';
+      this.queryUrl = this.extractQueryUrl(responseObject);
+    } else {
+      this.summaryHTML = null;
+      this.thumbnailSrc = '';
+    }
+  }
+}
+
+class RunescapeAPICall extends BaseWikiAPICall {
+  constructor() {
+    super("https://runescape.wiki/api.php?");
+  }
+
+  // returns an object
+  extractSpecificObject (responseJson) {
+    return responseJson["query"]["pages"][0];
+  }
+
+  // returns a string or undefined
+  extractExtract(responseObject) {
+    return responseObject["extract"];
+  }
+
+  // returns a string or undefined
+  extractThumbnail(responseObject) {
+    const thumbnailObj = responseObject["thumbnail"];
+    return thumbnailObj?.["source"];
+  }
+
+  extractQueryUrl(responseObject) {
+    return responseObject["fullurl"];
+  }
+
+  generateQueryUrl(query) {
+    const params = new URLSearchParams({
+      "action": "query",
+      "format": "json",
+      "prop": "info|extracts|pageimages",
+      "formatversion": 2,
+      "redirects": true,
+      "exintro": true,
+      "exchars": 400,
+      // "explaintext": true,
+      // "exsectionformat": "plain",
+      "piprop": "thumbnail",
+      "pithumbsize": 400,
+      "pilicense": "any",
+      "inprop": "url",
+      "titles": `${query}`,
+      "smaxage": 300,
+      "maxage": 300,
+      "uselang": "content",
+    });
+
+    return this.url  + params;
+  }
+}
+
+class WikipediaAPICall extends BaseWikiAPICall {
+  constructor() {
+    super("https://en.wikipedia.org/api/rest_v1");
+  }
+
+  // returns an object
+  extractSpecificObject (responseJson) {
+    // does nothing in this case
+    // TODO/REVIEW: review the purpose of this function
+    return responseJson;
+  }
+
+  // returns a string or undefined
+  extractExtract(responseObject) {
+    return responseObject["extract_html"];
+  }
+
+  // returns a string or undefined
+  extractThumbnail(responseObject) {
+    const thumbnailObj = responseObject["thumbnail"];
+    return thumbnailObj?.["source"];
+  }
+
+  extractQueryUrl(responseObject) {
+    return responseObject["content_urls"]["desktop"]["page"];
+  }
+
+  generateQueryUrl(query) {
+    return `${this.url}/page/summary/${query}`;
+  }
+}
+
 import {
   computePosition,
   autoPlacement,
@@ -24,6 +166,7 @@ tooltip.addEventListener('transitionend', () => {
 
 changeElement.addEventListener('input', (e) => {
   wikiLinks[0].innerHTML = e.target.value;
+  wikiLinks[1].innerHTML = e.target.value;
 })
 
 function showTooltip() {
@@ -114,62 +257,26 @@ tooltip.addEventListener('pointerleave', () => {
   }, linkTransTime);
 })
 
+const wikiQueryMap = {
+  "runescape": new RunescapeAPICall(),
+  "wikipedia": new WikipediaAPICall()
+};
+
 async function getWikiExtract(domElement) {
   const link = domElement.target.innerHTML;
 
   // REVIEW: consider using revisions to cache dom changes.
   // for now, just TODO: aggressively cache.
   // rvprop: timestamp + prop: revisions
-  const params = new URLSearchParams({
-    "action": "query",
-    "format": "json",
-    "prop": "info|extracts|pageimages",
-    "formatversion": 2,
-    "redirects": true,
-    "exintro": true,
-    "exchars": 400,
-    // "explaintext": true,
-    // "exsectionformat": "plain",
-    "piprop": "thumbnail",
-    "pithumbsize": 400,
-    "pilicense": "any",
-    "inprop": "url",
-    "titles": `${link}`,
-    "smaxage": 300,
-    "maxage": 300,
-    "uselang": "content",
-  });
 
-  let response = await fetch("https://runescape.wiki/api.php?" + params);
-  let responseJson = await response.json();
-  // console.log(responseJson);
-  const objContent = responseJson["query"]["pages"][0];
+  let queryWiki = wikiQueryMap[domElement.target.getAttribute("query-wiki")];
 
-  domElement.target.href = objContent["fullurl"];
-  // console.log(objContent["extract"]);
-  // FIXME: find better solution???
-  // replace breaking newline, just one should be enough... i guess
-  // do this because newlines aren't consistent with the rest of the line heights and introduce a weird
-  // clipping effect. this allows to have (hopefully) all extracts end at 9 lines
-  // JANK
-  // idk how the rswiki does it, they get plaintext but their thing has formatting... probably some preprocessing..
-  // in plaintext this isn't a problem since it ignores all newlines
-  const extract = objContent["extract"];
-  if (extract) {
-    tooltipText.innerHTML = extract.replace('\n</p><p>', ' ');
-    let thumbnail = objContent["thumbnail"];
-    if (thumbnail) {
-      let imgSource = thumbnail["source"];
-      tooltipThumb.setAttribute('src', imgSource);
-    } else {
-      // in css this also sets the display: none,
-      // so the grid adjusts
-      tooltipThumb.setAttribute('src', '');
-    }
-  } else {
-    tooltipText.innerHTML = `<p>Query "<b>${link}</b>" not found.</p>`;
-    tooltipThumb.setAttribute('src', '');
-  }
+  //
+  await queryWiki.generateJsonResponse(link);
+  domElement.target.href = queryWiki.queryUrl;
+  tooltipText.innerHTML = queryWiki.summaryHTML ?? `<p>Query "<b>${link}</b>" not found.</p>`;
+  tooltipThumb.setAttribute('src', queryWiki.thumbnailSrc);
+
 
   computePosition(domElement.target, tooltip, {
     middleware: [shift({ padding: 5 }), autoPlacement(),
